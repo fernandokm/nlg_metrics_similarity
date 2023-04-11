ae090b9200bf0494636499709b17de7621b5ba8c
Victor Amaral <victoramaral@usp.br>
2023-04-11 12:22:05 +0200

    add chrf, rouge and ter metrics

--- src/metrics.py
+++ src/metrics.py
@@ -12,13 +12,77 @@ def evaluate_bleu(candidates : list[list[str]], references : list[str]):
         - scores : List[float]
     """
     scores = []
-    bleu = evaluate.load("bleu")
+    metric = evaluate.load("bleu")
     for pred, refs in zip(candidates, references):
         pred = [pred]
         refs = [refs]
-        result = bleu.compute(predictions=pred, references=refs)
+        result = metric.compute(predictions=pred, references=refs)
         scores.append(result['bleu'])
 
     return scores
+
+
+def evaluate_chrf(candidates : list[list[str]], references : list[str]):
+    """
+    Compute chrf metric for a batch of sentences.
+    params:
+        - cands : List[str]
+        - refs List[List[str]]
+    
+    output:
+        - scores : List[float]
+    """
+    scores = []
+    metric = evaluate.load("chrf")
+    for pred, refs in zip(candidates, references):
+        pred = [pred]
+        refs = [refs]
+        result = metric.compute(predictions=pred, references=refs)
+        scores.append(result['score'])
+
+    return scores
+
+
+def evaluate_rouge(candidates : list[list[str]], references : list[str]):
+    """
+    Compute rouge metric for a batch of sentences.
+    params:
+        - cands : List[str]
+        - refs List[List[str]]
+    
+    output:
+        - scores : List[float]
+    """
+    scores = []
+    metric = evaluate.load("rouge")
+    for pred, refs in zip(candidates, references):
+        pred = [pred]
+        refs = [refs]
+        result = metric.compute(predictions=pred, references=refs)
+        scores.append(result['rouge1'])
+
+    return scores
+
+
+def evaluate_ter(candidates : list[list[str]], references : list[str]):
+    """
+    Compute ter metric for a batch of sentences.
+    params:
+        - cands : List[str]
+        - refs List[List[str]]
+    
+    output:
+        - scores : List[float]
+    """
+    scores = []
+    metric = evaluate.load("ter")
+    for pred, refs in zip(candidates, references):
+        pred = [pred]
+        refs = [refs]
+        result = metric.compute(predictions=pred, references=refs)
+        scores.append(result['score'])
+
+    return scores
+
 
 
