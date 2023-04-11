21317dbb4604a9b9c829dfd32af8aa58a8353367
Victor Amaral <victoramaral@usp.br>
2023-04-11 14:10:36 +0200

    meteor and moverscore implemented

--- src/metrics.py
+++ src/metrics.py
@@ -84,5 +84,56 @@ def evaluate_ter(candidates : list[list[str]], references : list[str]):
 
     return scores
 
+def evaluate_meteor(candidates : list[list[str]], references : list[str]):
+    """
+    Compute meteor metric for a batch of sentences.
+    params:
+        - cands : List[str]
+        - refs List[List[str]]
+    
+    output:
+        - scores : List[float]
+    """
+    scores = []
+    metric = evaluate.load("meteor")
+    for pred, refs in zip(candidates, references):
+        pred = [pred]
+        refs = [refs]
+        result = metric.compute(predictions=pred, references=refs)
+        scores.append(result['meteor'])
+
+    return scores
+
+
+def evaluate_moverscore(candidates : list[list[str]], references : list[str]):
+    """
+    Compute moverscore metric for a batch of sentences.
+    params:
+        - cands : List[str]
+        - refs List[List[str]]
+    
+    output:
+        - scores : List[float]
+    """
+    from moverscore import word_mover_score, defaultdict
+    list_scores = []
+    
+    for pred, refs in zip(candidates, references):
+
+        idf_dict_hyp = defaultdict(lambda: 1.)
+        idf_dict_ref = defaultdict(lambda: 1.)
+    
+        pred = [pred] * len(refs)
+    
+        sentence_score = 0 
+
+        scores = word_mover_score(refs, pred, idf_dict_ref, idf_dict_hyp, stop_words=[], n_gram=1, remove_subwords=False)
+    
+        sentence_score = np.mean(scores)
+
+        list_scores.append(sentence_score)
+
+    return list_scores
+
 
 
