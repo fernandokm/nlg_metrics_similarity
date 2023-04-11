9b3adbded852ca0e48c7b38654582ce88c5ccc83
Fernando Matsumoto <ferkmatsumoto@gmail.com>
2023-04-11 18:24:42 +0200

    Add caching to the metrics

--- src/metrics.py
+++ src/metrics.py
@@ -1,12 +1,18 @@
+import base64
+import functools
+import hashlib
+import json
 from functools import cache
-from typing import Optional
+from pathlib import Path
+from typing import Callable, Optional
 
 import evaluate
 import numpy as np
 import transformers
 
+DATA_DIR = Path("data/metrics")
+
 DEFAULT_MODEL = "distilbert-base-multilingual-cased"
-hidden_layers = None
 
 
 @cache
@@ -16,7 +22,50 @@ def get_model_hidden_layers(model_name):
     return model.model.config.num_hidden_layers
 
 
-def evaluate_bleu(candidates: list[str], references: list[list[str]]) -> list[str]:
+def cache_metric_to_disk(
+    fn: Callable[[list[str], list[list[str]], str | None], list[float]]
+):
+    """Enables caching for a metric function.
+
+    This decorator allows a metric function to be cached to disk
+    (to the directory data/metrics), so it only has to be computed once
+    for any given arguments.
+    """
+
+    @functools.wraps(fn)
+    def wrapper(
+        candidates: list[str], references: list[list[str]], model: str | None = None
+    ) -> list[float]:
+        # The function is only recomputed if any of the following elements change
+        key_hasher = hashlib.sha256(
+            json.dumps(
+                {
+                    "candidates": candidates,
+                    "references": references,
+                    "model": model,
+                }
+            ).encode("utf-8")
+        )
+        key = base64.urlsafe_b64encode(key_hasher.digest()).decode("utf-8")
+        cache_file = DATA_DIR / f"{fn.__module__}.{fn.__qualname__}.{key}.json"
+
+        DATA_DIR.mkdir(exist_ok=True, parents=True)
+
+        if cache_file.exists():
+            return json.load(cache_file.open("r"))
+
+        result = fn(candidates, references, model)
+        json.dump(result, cache_file.open("w"))
+
+        return result
+
+    return wrapper
+
+
+@cache_metric_to_disk
+def evaluate_bleu(
+    candidates: list[str], references: list[list[str]], model: str | None = None
+) -> list[float]:
     """
     Compute bleu metric for a batch of sentences.
     params:
@@ -38,7 +87,10 @@ def evaluate_bleu(candidates: list[str], references: list[list[str]]) -> list[st
     return scores
 
 
-def evaluate_chrf(candidates: list[str], references: list[list[str]]) -> list[str]:
+@cache_metric_to_disk
+def evaluate_chrf(
+    candidates: list[str], references: list[list[str]], model: str | None = None
+) -> list[float]:
     """
     Compute chrf metric for a batch of sentences.
     params:
@@ -60,7 +112,10 @@ def evaluate_chrf(candidates: list[str], references: list[list[str]]) -> list[st
     return scores
 
 
-def evaluate_rouge(candidates: list[str], references: list[list[str]]) -> list[str]:
+@cache_metric_to_disk
+def evaluate_rouge(
+    candidates: list[str], references: list[list[str]], model: str | None = None
+) -> list[float]:
     """
     Compute rouge metric for a batch of sentences.
     params:
@@ -82,7 +137,10 @@ def evaluate_rouge(candidates: list[str], references: list[list[str]]) -> list[s
     return scores
 
 
-def evaluate_ter(candidates: list[str], references: list[list[str]]) -> list[str]:
+@cache_metric_to_disk
+def evaluate_ter(
+    candidates: list[str], references: list[list[str]], model: str | None = None
+) -> list[float]:
     """
     Compute ter metric for a batch of sentences.
     params:
@@ -104,7 +162,10 @@ def evaluate_ter(candidates: list[str], references: list[list[str]]) -> list[str
     return scores
 
 
-def evaluate_meteor(candidates: list[str], references: list[list[str]]) -> list[str]:
+@cache_metric_to_disk
+def evaluate_meteor(
+    candidates: list[str], references: list[list[str]], model: str | None = None
+) -> list[float]:
     """
     Compute meteor metric for a batch of sentences.
     params:
@@ -126,9 +187,10 @@ def evaluate_meteor(candidates: list[str], references: list[list[str]]) -> list[
     return scores
 
 
+@cache_metric_to_disk
 def evaluate_moverscore(
     candidates: list[str], references: list[list[str]], model: Optional[str] = None
-) -> list[str]:
+) -> list[float]:
     """
     Compute moverscore metric for a batch of sentences.
     params:
@@ -170,9 +232,10 @@ def evaluate_moverscore(
     return list_scores
 
 
+@cache_metric_to_disk
 def evaluate_bertscore(
     candidates: list[str], references: list[list[str]], model: Optional[str] = None
-) -> list[str]:
+) -> list[float]:
     """
     Compute bertscore metric for a batch of sentences.
     params:
@@ -198,9 +261,10 @@ def evaluate_bertscore(
     return list_scores
 
 
+@cache_metric_to_disk
 def evaluate_baryscore(
     candidates: list[str], references: list[list[str]], model: Optional[str] = None
-) -> list[str]:
+) -> list[float]:
     """
     Compute Baryscore for a batch of sentences
     (compute for each reference and take the best score).
@@ -232,9 +296,10 @@ def evaluate_baryscore(
     return list_scores
 
 
+@cache_metric_to_disk
 def evaluate_depthscore(
     candidates: list[str], references: list[list[str]], model: Optional[str] = None
-) -> list[str]:
+) -> list[float]:
     """
     Compute DepthScore for a batch of sentences
     (compute for each reference and take the best score).
